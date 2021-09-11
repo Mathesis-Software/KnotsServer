d9eae22ca0edf6d0c388bec5df975d84f895dc6f
Nikolay Pultsin <geometer@fbreader.org>
2021-09-11 22:28:58 +0000

    cleanup

--- api/views.py
+++ api/views.py
@@ -3,7 +3,6 @@ import json
 from spherogram.links import Link
 from spherogram.links.orthogonal import OrthogonalLinkDiagram
 
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
