7452a9dbbbdb4448c55d81f3b66459fc3d85a43a
Nikolay Pultsin <geometer@fbreader.org>
2021-09-11 22:20:18 +0000

    DATABASES settings moved to private part

--- knots/settings.py
+++ knots/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from .local import SECRET_KEY, DEBUG, ALLOWED_HOSTS
+from .local import SECRET_KEY, DEBUG, ALLOWED_HOSTS, DATABASES
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -61,18 +61,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'knots.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
