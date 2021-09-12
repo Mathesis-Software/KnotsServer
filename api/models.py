c282be4cd14558570d375dc1d5d524ec3046090c
Nikolay Pultsin <geometer@fbreader.org>
2021-09-12 21:38:22 +0000

    dtcode max length: 128 => 19

--- api/models.py
+++ api/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Knot(models.Model):
     identifier = models.CharField(max_length=16, unique=True)
-    dtcode = models.CharField(max_length=128)
+    dtcode = models.CharField(max_length=19)
