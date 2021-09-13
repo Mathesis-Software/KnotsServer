35812aff9a3ef701b569cb1dd3d951b2a0b74a77
Nikolay Pultsin <geometer@fbreader.org>
2021-09-13 16:30:13 +0000

    syntax => search

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.page),
     path('about', views.page, kwargs={'page_id': 'about'}),
-    path('help/syntax', views.page, kwargs={'page_id': 'help/syntax'}),
+    path('help/search', views.page, kwargs={'page_id': 'help/search'}),
 ]
