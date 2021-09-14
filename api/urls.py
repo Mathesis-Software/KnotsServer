da57e23821c37e470bf4cf1f8f7527e902659d17
Nikolay Pultsin <geometer@fbreader.org>
2021-09-14 00:22:57 +0000

    callback renamed: diagram4Code => diagram

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('diagram4code', views.diagram4Code),
+    path('diagram', views.diagram4Code),
     path('test/<str:code>', views.test),
 ]
